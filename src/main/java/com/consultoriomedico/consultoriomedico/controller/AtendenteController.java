1d9923a3fd9a302b2b15da1c7cf030487c55a3d8
EDILSON VALENTIM DOS SANTOS BITENCOURT <edilson.bitencourt@prof.sc.senac.br>
2022-09-28 11:05:06 -0300

    Inserindo atendente

--- src/main/java/com/consultoriomedico/consultoriomedico/controller/AtendenteController.java
+++ src/main/java/com/consultoriomedico/consultoriomedico/controller/AtendenteController.java
@@ -3,6 +3,7 @@ package com.consultoriomedico.consultoriomedico.controller;
 import com.consultoriomedico.consultoriomedico.model.AtendenteEntity;
 import com.consultoriomedico.consultoriomedico.repository.AtendenteRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,16 +11,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value="/atendente")
 public class AtendenteController {
     
-    private AtendenteRepository repositorio;
-    @PostMapping(value="nome")
-    public int add(@RequestBody String nome){
+    @Autowired
+    private AtendenteRepository repositorio;    
+    
+    @PostMapping
+    public int add(@RequestBody AtendenteEntity atentendeREST){
         AtendenteEntity atendente = new AtendenteEntity();
-        atendente.setNome(nome);
+        atendente.setNome(atentendeREST.getNome());
 
         this.repositorio.save(atendente);
         return atendente.getId();
